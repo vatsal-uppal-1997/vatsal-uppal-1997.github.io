31df18cf9653dae54c985ed7719c544eb711bae9
vatsal-uppal-1997 <vatsal.uppal@gmail.com>
2019-02-11 14:07:48 +0530

    dashboard looks mostly done

--- views/scripts/home.js
+++ views/scripts/home.js
@@ -2,13 +2,18 @@ function toggle() {
     const hookAside = document.querySelector("#hookAside");
     const hide = document.querySelector("#hide");
     const show = document.querySelector("#show");
+    const content = document.querySelector("#contentHook");
     if (hookAside.style.display === "none") {
         hookAside.style.display = "flex";
         hide.style.display = "inline-block";
         show.style.display = "none";
+        content.style.transform = "translate(20rem)";
+        content.style.marginRight = "20rem";
     } else {
         hookAside.style.display = "none";
         show.style.display = "inline-block";
         hide.style.display = "none";
+        content.style.transform = "unset";
+        content.style.marginRight = "0";
     }
 }
